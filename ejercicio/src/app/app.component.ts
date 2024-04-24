485b111c22d7376ccb555f367c3d308bc58b6c91
ChristianRodrigo98 <christian.rodrigoalvarez@cieep.com>
2024-04-25 01:01:08 +0200

    Mi ejercicio (estoy hasta la polla no puedo mas)

--- ejercicio/src/app/app.component.ts
+++ ejercicio/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChristianComponent } from './component/christian/christian.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChristianComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
