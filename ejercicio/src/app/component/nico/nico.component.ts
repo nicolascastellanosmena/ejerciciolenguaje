cd810641fe36f6571cc9d714b11f2f50d3fa47bf
Nico <nicolas96vlc@gmail.com>
2024-04-25 18:26:25 +0200

    Se han modificado los archivos y se ha creado la api digimon bakanita

--- ejercicio/src/app/component/nico/nico.component.ts
+++ ejercicio/src/app/component/nico/nico.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiNicoService } from '../../data/api-nico.service';
+import { digimon } from '../../common/nico';
 
 @Component({
   selector: 'app-nico',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './nico.component.html',
   styleUrl: './nico.component.css'
 })
-export class NicoComponent {
+export class NicoComponent implements OnInit{
+  digimon !: digimon 
+  constructor(private servicio:ApiNicoService){}
 
+  ngOnInit(): void {
+    this.getLoad()  
+  }
+  getLoad() {
+    this.servicio.getData().subscribe(
+      (next:digimon)=>{
+        this.digimon=next
+      },
+      (error)=>{
+        console.log("error");
+      },
+      ()=>{
+        console.log("completed");
+      }
+    );
+  }
+    
 }
--- ejercicio/src/app/data/api-nico.service.ts
+++ ejercicio/src/app/data/api-nico.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { digimon } from '../common/nico';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiNicoService {
 
-  constructor() { }
+  constructor(private dataService: HttpClient) { }
+   getData(): Observable<digimon>{
+
+    return this.dataService.get<digimon>("assets/digimon.json")
+  }
+
+
 }
